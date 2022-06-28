10cb86a579c17d03a3d8756de0871a937b8321d8
Relari <renzolavador@gmail.com>
2022-06-27 23:35:34 -0500

    test: updated unit test

---
+++ src/test/java/com/co/pragma/training/service/app/PersonManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.pragma.training.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
