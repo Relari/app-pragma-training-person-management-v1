1a5639c5193cbd0405311666f7660456468f1565
Relari <renzolavador@gmail.com>
2022-06-29 12:05:42 -0500

    feat: The configuration of the eureka client is added.

--- src/main/java/com/co/pragma/training/service/app/infrastructure/config/spring/PersonManagementApplication.java
+++ src/main/java/com/co/pragma/training/service/app/infrastructure/config/spring/PersonManagementApplication.java
@@ -2,7 +2,9 @@ package com.co.pragma.training.service.app.infrastructure.config.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.co.pragma.training.service.app.infrastructure")
 public class PersonManagementApplication {
 
