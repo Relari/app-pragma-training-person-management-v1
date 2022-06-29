4fd4fdf4ec8af782a770db607661a5b88ccdabc0
Relari <renzolavador@gmail.com>
2022-06-29 12:01:11 -0500

    feat: The use case beans are declared.

--- src/main/java/com/co/pragma/training/service/app/infrastructure/config/spring/SpringConfiguration.java
+++ src/main/java/com/co/pragma/training/service/app/infrastructure/config/spring/SpringConfiguration.java
@@ -2,7 +2,11 @@ package com.co.pragma.training.service.app.infrastructure.config.spring;
 
 import com.co.pragma.training.service.app.application.dao.PersonDao;
 import com.co.pragma.training.service.app.application.usecase.PersonManagementCreateService;
+import com.co.pragma.training.service.app.application.usecase.PersonManagementSearchOlderAgesService;
+import com.co.pragma.training.service.app.application.usecase.PersonManagementSearchService;
 import com.co.pragma.training.service.app.application.usecase.impl.PersonManagementCreateServiceImpl;
+import com.co.pragma.training.service.app.application.usecase.impl.PersonManagementSearchOlderAgesServiceImpl;
+import com.co.pragma.training.service.app.application.usecase.impl.PersonManagementSearchServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -17,7 +21,16 @@ public class SpringConfiguration {
         return new PersonManagementCreateServiceImpl(personDao);
     }
 
+    @Bean
+    public PersonManagementSearchService personManagementSearchService(
+            PersonDao personDao) {
+        return new PersonManagementSearchServiceImpl(personDao);
+    }
 
-
+    @Bean
+    public PersonManagementSearchOlderAgesService personManagementSearchOlderAgesService(
+            PersonDao personDao) {
+        return new PersonManagementSearchOlderAgesServiceImpl(personDao);
+    }
 
 }
